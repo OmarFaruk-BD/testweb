6c49d7f5e3ea2c69f2332e2de66b06db4fd8b8cb
Omar Faruk <omarfaruk2bd@gmail.com>
2025-03-20 16:40:09 +0600

    Updated

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,18 +3,20 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a67a10d3522f59c042141663a14ee0d4",
+const RESOURCES = {"flutter_bootstrap.js": "7cda90955f41b16e165b7f42701b0a3c",
 "version.json": "0f50932c16e95482597cbc81f7d9fe17",
 "index.html": "8ba4ff2e7f711ae76a6b191bb3a5550b",
 "/": "8ba4ff2e7f711ae76a6b191bb3a5550b",
 "main.dart.js": "ccda6156a9429d1133a9efdf71b8e0f9",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"main.dart.mjs": "4d1dfe9acdde3a471da62eb8f4facde6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "ff06da562c8b0da0477afcdca20dba9f",
+"main.dart.wasm": "c8b08cf386646c116b4b7345a3452cd0",
 "assets/AssetManifest.json": "8f01e186a425bf8ff271c1de0e8deeb7",
 "assets/NOTICES": "76f246227eccf6c20dbba167cd4fbee5",
 "assets/FontManifest.json": "02449f5dcf23ed694ea877c32c1e0bb8",
@@ -51,6 +53,8 @@ const RESOURCES = {"flutter_bootstrap.js": "a67a10d3522f59c042141663a14ee0d4",
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
+"main.dart.wasm",
+"main.dart.mjs",
 "index.html",
 "flutter_bootstrap.js",
 "assets/AssetManifest.bin.json",
